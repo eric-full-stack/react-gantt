554c803798c08f584ec411a4b9ad8c38a3f25299
Eric <ericrprates@gmail.com>
2026-04-08 18:28:43 -0300

    fix: milestones, day view mode

--- dist/types/components/table-body/cell-renderers.d.ts
+++ dist/types/components/table-body/cell-renderers.d.ts
@@ -5,15 +5,15 @@ interface CellRendererProps<RecordType = DefaultRecordType> {
     record: Gantt.Record<RecordType>;
     column: Gantt.Column<RecordType>;
 }
-export declare const TextCell: <RecordType extends DefaultRecordType>({ value, }: CellRendererProps<RecordType>) => JSX.Element;
-export declare const DateCell: <RecordType extends DefaultRecordType>({ value, column, }: CellRendererProps<RecordType>) => JSX.Element;
-export declare const NumberCell: <RecordType extends DefaultRecordType>({ value, column, }: CellRendererProps<RecordType>) => JSX.Element;
-export declare const ProgressCell: <RecordType extends DefaultRecordType>({ value, }: CellRendererProps<RecordType>) => JSX.Element;
-export declare const StatusCell: <RecordType extends DefaultRecordType>({ value, }: CellRendererProps<RecordType>) => JSX.Element;
-export declare const PriorityCell: <RecordType extends DefaultRecordType>({ value, }: CellRendererProps<RecordType>) => JSX.Element;
-export declare const UserCell: <RecordType extends DefaultRecordType>({ value, }: CellRendererProps<RecordType>) => JSX.Element;
-export declare const TagsCell: <RecordType extends DefaultRecordType>({ value, }: CellRendererProps<RecordType>) => JSX.Element;
-export declare const CurrencyCell: <RecordType extends DefaultRecordType>({ value, column, }: CellRendererProps<RecordType>) => JSX.Element;
-export declare const CustomFieldCell: <RecordType extends DefaultRecordType>({ value, record, column, }: CellRendererProps<RecordType>) => JSX.Element;
+export declare const TextCell: <RecordType extends DefaultRecordType>({ value, }: CellRendererProps<RecordType>) => React.JSX.Element;
+export declare const DateCell: <RecordType extends DefaultRecordType>({ value, column, }: CellRendererProps<RecordType>) => React.JSX.Element;
+export declare const NumberCell: <RecordType extends DefaultRecordType>({ value, column, }: CellRendererProps<RecordType>) => React.JSX.Element;
+export declare const ProgressCell: <RecordType extends DefaultRecordType>({ value, }: CellRendererProps<RecordType>) => React.JSX.Element;
+export declare const StatusCell: <RecordType extends DefaultRecordType>({ value, }: CellRendererProps<RecordType>) => React.JSX.Element;
+export declare const PriorityCell: <RecordType extends DefaultRecordType>({ value, }: CellRendererProps<RecordType>) => React.JSX.Element;
+export declare const UserCell: <RecordType extends DefaultRecordType>({ value, }: CellRendererProps<RecordType>) => React.JSX.Element;
+export declare const TagsCell: <RecordType extends DefaultRecordType>({ value, }: CellRendererProps<RecordType>) => React.JSX.Element;
+export declare const CurrencyCell: <RecordType extends DefaultRecordType>({ value, column, }: CellRendererProps<RecordType>) => React.JSX.Element;
+export declare const CustomFieldCell: <RecordType extends DefaultRecordType>({ value, record, column, }: CellRendererProps<RecordType>) => React.JSX.Element;
 export declare const renderCell: <RecordType extends DefaultRecordType>(value: any, record: Gantt.Record<RecordType>, column: Gantt.Column<RecordType>) => React.ReactNode;
 export {};
